6e71e885417a43e2609ce99116871b48ea945a09
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-07-09 21:01:56 +0100

    factory bot set up

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
-# require 'rails_helper'
-# RSpec.describe PostsController, type: :controller do
-#   describe 'GET /new ' do
-#     it 'responds with 200' do
-#       get :new
-#       expect(response).to have_http_status(200)
-#     end
-#   end
+require 'rails_helper'
+ RSpec.describe PostsController, type: :controller do
+
+  describe 'GET /new ' do
+    it 'responds with 200' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
 
 #   describe 'POST /' do
 #     it 'responds with 200' do
@@ -50,4 +51,4 @@
 #       expect(Post.find_by(message: 'Hello, world!')).to be
 #     end
 #   end
-# end
+ end
