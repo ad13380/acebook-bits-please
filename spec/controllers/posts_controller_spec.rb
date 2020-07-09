e9842ad07d7dceaf2ad6619b4ca8c23ff550d1cf
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-07-09 22:20:51 +0100

    post controller tests done

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -9,22 +9,9 @@ require 'rails_helper'
   let(:invalid_attributes) {
     skip("Add a hash of attributes invalid for your model")
   }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # UsersController. Be sure to keep this updated too.
+  
   let(:valid_session) { {} }
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new User" do
-        expect {
-          post :create, params: {user: valid_attributes}, session: valid_session
-        }.to change(User, :count).by(1)
-      end
-    end
-  end
- 
   describe 'GET /new ' do
 
     it 'responds with 302' do
@@ -33,53 +20,75 @@ require 'rails_helper'
     end
   end
 
-  # describe "#create" do
-  #   it "creates a post" do
-  #     expect(Post).to receive(:create)
-  #     get :create
-  #   end
-  # end
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new User" do
+        expect {
+          post :create, params: {user: valid_attributes}, session: valid_session
+        }.to change(User, :count).by(1)
+      end
+      it "redirects to the post wall" do
+        post :create, params: {user: valid_attributes}, session: valid_session
+        expect(response).to redirect_to(post_path)
+      end
+    end
+  end
 
-  # describe 'POST /' do
-    # it 'redirects user to post page' do
-    #   post :create, params: { post: { message: 'Hello, world!' } }
-    #   expect(response).to redirect_to(posts_url)
-    # end
+  describe "#create" do
+    it "creates a post" do
+      user = User.create! valid_attributes
+      expect(Post).to receive(:create)
+      get :create
+    end
+  end
 
-  #   it 'creates a post' do
-  #     post :create, params: { post: { message: 'Hello, world!' } }
-  #     expect(Post.find_by(message: 'Hello, world!')).to be
-  #   end
-  # end
+  describe 'POST /' do
+    it 'redirects user to post page' do
+      user = User.create! valid_attributes
+      post :create, params: { post: { message: 'Hello, world!' } }
+      expect(response).to redirect_to(posts_url)
+    end
+  end
 
-#   describe 'GET /' do
-#     it 'responds with 200' do
-#       get :index
-#       expect(response).to have_http_status(200)
-#     end
-#   end
+    it 'creates a post' do
+      user = User.create! valid_attributes
+      post :create, params: { post: { message: 'Hello, world!' } }
+      expect(Post.find_by(message: 'Hello, world!')).to be
+    end
 
-#   describe 'POST /' do
-#     it 'responds with 200' do
-#       post :create, params: { post: { message: 'Hello, world!' } }
-#       expect(response).to redirect_to(posts_url)
-#     end
+  describe 'GET /' do
+    it 'responds with 200' do
+      user = User.create! valid_attributes
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
 
-#     it 'creates a post' do
-#       post :create, params: { post: { message: 'Hello, world!' } }
-#       expect(Post.find_by(message: 'Hello, world!')).to be
-#     end
-#   end
+  describe 'POST /' do
+    it 'responds with 200' do
+      user = User.create! valid_attributes
+      post :create, params: { post: { message: 'Hello, world!' } }
+      expect(response).to redirect_to(posts_url)
+    end
 
-#   describe 'POST /' do
-#     it 'responds with 200' do
-#       post :create, params: { post: { message: 'Hello, world!' } }
-#       expect(response).to redirect_to(posts_url)
-#     end
+    it 'creates a post' do
+      user = User.create! valid_attributes
+      post :create, params: { post: { message: 'Hello, world!' } }
+      expect(Post.find_by(message: 'Hello, world!')).to be
+    end
+  end
 
-#     it 'creates a post' do
-#       post :create, params: { post: { message: 'Hello, world!' } }
-#       expect(Post.find_by(message: 'Hello, world!')).to be
-#     end
-#   end
+  describe 'POST /' do
+    it 'responds with 200' do
+      user = User.create! valid_attributes
+      post :create, params: { post: { message: 'Hello, world!' } }
+      expect(response).to redirect_to(posts_url)
+    end
+
+    it 'creates a post' do
+      user = User.create! valid_attributes
+      post :create, params: { post: { message: 'Hello, world!' } }
+      expect(Post.find_by(message: 'Hello, world!')).to be
+    end
+  end
  end
