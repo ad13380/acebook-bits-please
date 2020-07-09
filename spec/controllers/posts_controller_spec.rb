f4fc4bdca6341ed3ff716aae77401807f4c4ed86
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-07-09 22:21:57 +0100

    refactor post tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -9,7 +9,7 @@ require 'rails_helper'
   let(:invalid_attributes) {
     skip("Add a hash of attributes invalid for your model")
   }
-  
+
   let(:valid_session) { {} }
 
   describe 'GET /new ' do
@@ -22,10 +22,10 @@ require 'rails_helper'
 
   describe "POST #create" do
     context "with valid params" do
-      it "creates a new User" do
+      it "creates a new post" do
         expect {
           post :create, params: {user: valid_attributes}, session: valid_session
-        }.to change(User, :count).by(1)
+        }.to change(Post, :count).by(1)
       end
       it "redirects to the post wall" do
         post :create, params: {user: valid_attributes}, session: valid_session
