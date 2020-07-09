ecd519ce742bce85bd79cd34cafae560a63043d4
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-07-09 23:30:01 +0100

    unit tests for post model

--- spec/views/posts/edit.html.erb_spec.rb
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -1,25 +1,38 @@
-# require 'rails_helper'
+require 'rails_helper'
 
 # RSpec.describe "posts/edit", type: :view do
-#   before(:each) do
-#     @posts = assign(:message, Post.create!())
-#   end
+# before(:each) do
+#   @user = assign(:user, User.create!(
+#     :firstname => "MyString",
+#     :lastname => "MyString",
+#     :email => "MyString@email.com",
+#     :password => "123456"
+  # ))
+  # @post = assign(:post, Post.create!(
+  #   :message => "im a post",
+  #   :user_id => "12345",
+  # ))
+# end
 
-#   it "renders the edit album form" do
-#     render
+# it "renders the edit post form" do
+#   assign(:post, Post.create(message: 'hello', id: "1"))
+#   render
 
-#     assert_select "form[action=?][method=?]", album_path(@album), "post" do
-#     end
+#   assert_select "form[action=?][method=?]", post_path(@post), "post" do
+
+#     assert_select "user[message]"
 #   end
+# end
 # end
 
 # describe 'products/show.html.erb' do
 #   it 'displays product details correctly' do
-#     assign(:message, Post.create(message: 'Shirt'))
+#     assign(:product, Product.create(name: 'Shirt', price: 50.0))
 
 #     render
 
 #     rendered.should contain('Shirt')
+#     rendered.should contain('50.0')
 #   end
 # end
-# end
+
