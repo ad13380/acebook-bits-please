1e416f3631f71558ab0dc8b9fe7b59272033da64
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-07-10 12:15:03 +0100

    fix feature tests

--- spec/features/user_can_delete_post_spec.rb
+++ spec/features/user_can_delete_post_spec.rb
@@ -23,23 +23,21 @@ RSpec.feature "Timeline", type: :feature do
   end
 
   # Need sign out button
-  # scenario "Can't delet someone else's post" do
-  #   fill_in "Message", with: "Hello, world!"
-  #   click_button "Submit"
-  #   click_button "Sign Out"
-
-  #   click_link "Sign Up"
-  #   fill_in "First Name", with: "test2firstname"
-  #   fill_in "Last Name", with: "test2lastname"
-  #   fill_in "Email", with: "test2@gmail.com"
-  #   fill_in "Password", with: "123456"
-  #   fill_in "Confirm Password", with: "123456"
-  #   click_button "Create User"
-  #   fill_in "Email", with: "test2@gmail.com"
-  #   fill_in "Password", with: "123456"
-  #   click_button "Login"
-
-  #   expect(page).not_to have_link("Delete")
-  # end
+  scenario "Can't delet someone else's post" do
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+  click_link "Log out"
+  click_link "Sign Up"
+  fill_in "user_firstname", with: "test2firstname"
+  fill_in "user_lastname", with: "testlastname"
+  fill_in "user_email", with: "test2@gmail.com"
+  fill_in "user_password", with: "123456"
+  fill_in "user_password_confirmation", with: "123456"
+  click_button "Create User"
+  fill_in "email", with: "test2@gmail.com"
+  fill_in "password", with: "123456"
+  click_button "Login"
+    expect(page).not_to have_link("Delete")
+  end
 
 end
--- spec/features/user_can_edit_post_spec.rb
+++ spec/features/user_can_edit_post_spec.rb
@@ -25,23 +25,22 @@ RSpec.feature "Timeline", type: :feature do
   end
 
   # Need sign out button
-  # scenario "Can't edit someone else's post" do
-  #   fill_in "Message", with: "Hello, world!"
-  #   click_button "Submit"
-  #   click_button "Sign Out"
+  scenario "Can't edit someone else's post" do
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+  click_link "Log out"
+  click_link "Sign Up"
+  fill_in "user_firstname", with: "test2firstname"
+  fill_in "user_lastname", with: "testlastname"
+  fill_in "user_email", with: "test2@gmail.com"
+  fill_in "user_password", with: "123456"
+  fill_in "user_password_confirmation", with: "123456"
+  click_button "Create User"
+  fill_in "email", with: "test2@gmail.com"
+  fill_in "password", with: "123456"
+  click_button "Login"
 
-  #   click_link "Sign Up"
-  #   fill_in "First Name", with: "test2firstname"
-  #   fill_in "Last Name", with: "test2lastname"
-  #   fill_in "Email", with: "test2@gmail.com"
-  #   fill_in "Password", with: "123456"
-  #   fill_in "Confirm Password", with: "123456"
-  #   click_button "Create User"
-  #   fill_in "Email", with: "test2@gmail.com"
-  #   fill_in "Password", with: "123456"
-  #   click_button "Login"
-
-  #   expect(page).not_to have_link("Edit")
-  # end
+    expect(page).not_to have_link("Edit")
+  end
 
 end
